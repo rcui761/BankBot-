90b17e09d8dab0c786035398d48eb7ab6bbb7631
rcui761 <ambercui199528@gmail.com>
2017-11-28 18:41:05 +1300

    undo

--- api/RestClient.js
+++ api/RestClient.js
@@ -64,7 +64,6 @@ exports.getAppointment = function getData(url, session) {
         } else {
             //callback(body, session);// what the meaning of callback 
             var bookedAppointment = JSON.parse(body);
-            session.send(res);
             var allAppointmentdate = [];
             var allAppointmenttime = [];
             var allAppointmenttype = [];
